cb6d4c402703694e637b3f91aa09f90ea1537ef3
Hano <hinsul3113@gmail.com>
2019-02-11 23:22:36 -0800

    added loader component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 
 class App extends React.Component {
@@ -14,7 +15,7 @@ class App extends React.Component {
       );
     }
 
-  render() {
+    renderContent() {
       if (this.state.errorMsg && !this.state.lat) {
         return <div>Error: { this.state.errorMsg }</div>
       }
@@ -22,7 +23,15 @@ class App extends React.Component {
         return <SeasonDisplay lat={this.state.lat}/>
       }
 
-      return <div>Loading!...</div>
+      return <Loader message="Please accept location request"/>
+    }
+
+  render() {
+    return (
+      <div className="container">
+        { this.renderContent() }
+      </div>
+    )
   }
 }
 

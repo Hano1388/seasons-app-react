6abc6f481427eb8a1ae04c55d629a34e658b8052
Hano <hinsul3113@gmail.com>
2019-02-11 16:50:23 -0800

    refactored code and added a lifecycle method to load data

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-  constructor(props) {
-      super(props);
+  
+    state = { lat: null, errorMsg: '' };
 
-      this.state = { lat: null, errorMsg: '' };
-
+    componentDidMount() {
       window.navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({ lat: position.coords.latitude })
-        }, // first callback is the success
-        err => {
-          this.setState({ errorMsg: err.message });
-        } // second callback is the fail(error)
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ errorMsg: err.message })
       );
     }
 
